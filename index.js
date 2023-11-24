ce5df6a27078267d94f749ae2a007bce48eff564
rashednyn <rashedmahamud22@gmail.com>
2023-11-25 01:50:01 +0600

    make admin related api creted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oz2ylzg.mongodb.net/?retryWrites=true&w=majority  `;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,6 +47,25 @@ async function run() {
             }
             const result = await userCollection.insertOne(user);
             res.send(result)
+        });
+        //patch in USER data
+        app.patch('/users/admin/:id',async(req,res)=>{
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };   
+            const updatedDoc={
+                $set:{
+                    role:'admin'
+                }
+            } 
+            const result = await userCollection.updateOne(filter,updatedDoc);
+            res.send(result);        
+        })
+        //delete USER
+        app.delete('/users/:id',async(req,res)=>{
+            const id =req.params.id;
+            const query = { _id:new ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
