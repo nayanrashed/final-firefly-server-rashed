18c8e32e59e3bc5fa1be8f2e601b83c49602add2
rashednyn <rashedmahamud22@gmail.com>
2023-11-25 03:19:11 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -31,39 +32,89 @@ async function run() {
         const postCollection = client.db("fireflyDb").collection("posts")
 
 
+        //JWT related API
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+            res.send({ token });
+        })
+
+        //middlewares
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized access" });
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "unauthorized access" })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        };
+        //use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: "forbidden access" });
+            }
+            next();
+        }
+
+
+
         //USER Related API
         //read all users data
-        app.get('/users', async(req,res)=>{
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
+        });
+        // getting user admin
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "forbidden access" })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin })
         })
         //create USER 
-        app.post('/users',async(req,res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email:user.email};
+            const query = { email: user.email };
             const existingUser = await userCollection.findOne(query);
-            if(existingUser){
+            if (existingUser) {
                 return res.send({ message: 'User already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result)
         });
         //patch in USER data
-        app.patch('/users/admin/:id',async(req,res)=>{
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };   
-            const updatedDoc={
-                $set:{
-                    role:'admin'
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
                 }
-            } 
-            const result = await userCollection.updateOne(filter,updatedDoc);
-            res.send(result);        
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
         })
         //delete USER
-        app.delete('/users/:id',async(req,res)=>{
-            const id =req.params.id;
-            const query = { _id:new ObjectId(id)};
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
@@ -79,9 +130,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('final firefly server is running....')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`firefly server is running on port ${port}`);
 })
